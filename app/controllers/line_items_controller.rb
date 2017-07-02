3b49558253b3f4ab32d03f60cccd9207fa7e2c43
Aleksander Wozniak <4wozniak@gmail.com>
2017-07-02 22:25:59 +0000

    Added deleting items from cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,9 +57,11 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @line_item = LineItem.find(params[:id])
     @line_item.destroy
+    
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Przedmiot usunięty z koszyka.' }
       format.json { head :no_content }
     end
   end
