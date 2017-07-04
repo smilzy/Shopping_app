34ee2977432460da4bda8659440e7386f89c22c0
Aleksander Wozniak <4wozniak@gmail.com>
2017-07-04 18:16:21 +0000

    Added ajax test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -49,4 +49,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to store_index_url
   end
+  
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:xiaomi).id },
+        xhr: true
+    end
+    
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Xiaomi/
+    end
+  end
 end
