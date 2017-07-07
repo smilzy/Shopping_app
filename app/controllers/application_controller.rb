c291ccc786752fb71b3f5b1a09929049658fd77e
Aleksander Wozniak <4wozniak@gmail.com>
2017-07-07 21:07:59 +0000

    Put last validation in wrong place instead of user model FIX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,4 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  after_destroy :ensure_an_admin_remains
-  class Error < StandardError
-  end
-  private
-  def ensure_an_admin_remains
-    if User.count.zero?
-      raise Error.new "Can't delete last user"
-    end
-  end
 end
